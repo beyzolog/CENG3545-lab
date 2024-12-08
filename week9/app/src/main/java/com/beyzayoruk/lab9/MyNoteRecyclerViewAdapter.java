fe80131b0429b3de881254a46a484e89ecd761ee
beyzolog <johnnydeep6587@gmail.com>
2024-12-08 14:58:37 +0300

    week9

---
+++ week9/app/src/main/java/com/beyzayoruk/lab9/MyNoteRecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package com.beyzayoruk.lab9;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.beyzayoruk.lab9.placeholder.PlaceholderContent.PlaceholderItem;
+import com.beyzayoruk.lab9.databinding.FragmentNoteBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyNoteRecyclerViewAdapter extends RecyclerView.Adapter<MyNoteRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Note> mValues;
+
+    public MyNoteRecyclerViewAdapter(List<Note> notes, NoteFragment.OnNoteListInteractionListener listener) {
+        mValues = notes;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentNoteBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentNoteBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
